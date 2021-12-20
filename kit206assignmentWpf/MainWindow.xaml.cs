539ebbaf75cd19cffc8cea11518350761565097b
AdamLordUtas <94874480+AdamLordUtas@users.noreply.github.com>
2021-12-21 09:39:43 +1100

    Update 21/12/2021 9:39am

--- kit206assignmentWpf/MainWindow.xaml.cs
+++ kit206assignmentWpf/MainWindow.xaml.cs
@@ -39,74 +39,116 @@ namespace kit206assignmentWpf
 
         private void researcherListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            Researcher selectedRes = (Researcher)researcherListBox.SelectedItem;
-            selectedRes = resControl.GetFullDetails(selectedRes.id);
+            try
+            {
+                Researcher selectedRes = (Researcher)researcherListBox.SelectedItem;
+                selectedRes = resControl.GetFullDetails(selectedRes.id);
 
-            resId.Content = selectedRes.id;
-            resType.Content = selectedRes.type;
-            resGivenName.Content = selectedRes.givenName;
-            resFamilyName.Content = selectedRes.familyName;
-            resTitle.Content = selectedRes.title;
-            resUnit.Content = selectedRes.unit;
-            resCampus.Content = selectedRes.campusSting();
-            resEmail.Content = selectedRes.email;
-            resDegree.Content = selectedRes.degree;
-            resSupervisor.Content = selectedRes.supervisorId;
-            resPosition.Content = selectedRes.position.Title();
+                resId.Content = selectedRes.id;
+                resType.Content = selectedRes.type;
+                resGivenName.Content = selectedRes.givenName;
+                resFamilyName.Content = selectedRes.familyName;
+                resTitle.Content = selectedRes.title;
+                resUnit.Content = selectedRes.unit;
+                resCampus.Content = selectedRes.campusSting();
+                resEmail.Content = selectedRes.email;
+                resDegree.Content = selectedRes.degree;
+                resSupervisor.Content = selectedRes.supervisorId;
+                resPosition.Content = selectedRes.position.Title();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading researcher details " + ex);
+                throw;
+            }
         }
 
 
         private void publicationListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            Publication selectedPub = (Publication)publicationListBox.SelectedItem;
-            selectedPub = pubControl.GetFullDetails(selectedPub);
+            try
+            {
+                Publication selectedPub = (Publication)publicationListBox.SelectedItem;
+                selectedPub = pubControl.GetFullDetails(selectedPub);
 
-            pubDoi.Content = selectedPub.doi;
-            pubTitle.Content = selectedPub.title;
-            pubAuthors.Content = selectedPub.authours;
-            pubYear.Content = selectedPub.year;
-            pubTitle.Content = selectedPub.type;
-            available.Content = selectedPub.available;
+                pubDoi.Content = selectedPub.doi;
+                pubTitle.Content = selectedPub.title;
+                pubAuthors.Content = selectedPub.authours;
+                pubYear.Content = selectedPub.year;
+                pubTitle.Content = selectedPub.type;
+                available.Content = selectedPub.available;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading publicatrion details " + ex);
+                //throw;
+            }
         }
 
         private void FetchPublications(object sender, RoutedEventArgs e)
         {
-            Researcher selectedRes = (Researcher)researcherListBox.SelectedItem;
-            pubControl.LoadPublications(selectedRes.id);
-            publicationListBox.ItemsSource = pubControl.displayList;
+            try
+            {
+                Researcher selectedRes = (Researcher)researcherListBox.SelectedItem;
+                pubControl.LoadPublications(selectedRes.id);
+                publicationListBox.ItemsSource = pubControl.displayList;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error fetching publications " + ex);
+                //throw;
+            }
+
+
         }
 
         private void levelFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            switch (levelFilter.SelectedIndex)
+            try
             {
-                case 1:
-                    resControl.FilterByLevel(Level.N);
-                    break;
-                case 2:
-                    resControl.FilterByLevel(Level.A);
-                    break;
-                case 3:
-                    resControl.FilterByLevel(Level.B);
-                    break;
-                case 4:
-                    resControl.FilterByLevel(Level.C);
-                    break;
-                case 5:
-                    resControl.FilterByLevel(Level.D);
-                    break;
-                case 6:
-                    resControl.FilterByLevel(Level.E);
-                    break;
-                default:
-                    resControl.ResetList();
-                    break;
+                switch (levelFilter.SelectedIndex)
+                {
+                    case 1:
+                        resControl.FilterByLevel(Level.N);
+                        break;
+                    case 2:
+                        resControl.FilterByLevel(Level.A);
+                        break;
+                    case 3:
+                        resControl.FilterByLevel(Level.B);
+                        break;
+                    case 4:
+                        resControl.FilterByLevel(Level.C);
+                        break;
+                    case 5:
+                        resControl.FilterByLevel(Level.D);
+                        break;
+                    case 6:
+                        resControl.FilterByLevel(Level.E);
+                        break;
+                    default:
+                        resControl.ResetList();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error filtering by level " + ex);
+                //throw;
             }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            resControl.FilterByName(researchSearchText.Text);
+            try
+            {
+                resControl.FilterByName(researchSearchText.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error with search " + ex);
+                //throw;
+            }         
         }
     }
 }
